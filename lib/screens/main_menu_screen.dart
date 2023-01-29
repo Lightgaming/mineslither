d3e710b1a2f407d68e4c0ba0b67e9882d76e6b67
Florian Lotz <florian.lotz@marqably.com>
2023-01-29 20:31:16 +0100

    add donate button + background image

--- lib/screens/main_menu_screen.dart
+++ lib/screens/main_menu_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_donation_buttons/flutter_donation_buttons.dart';
 import 'package:go_router/go_router.dart';
 import 'package:mineslither/widgets/app_bar.dart';
 
@@ -10,54 +11,72 @@ class MainMenuScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const GameAppBar(),
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton.icon(
-                onPressed: () {
-                  GoRouter.of(context).go('/play');
-                },
-                icon: const Icon(Icons.play_arrow),
-                label: const Text('Play'),
+      body: Stack(
+        children: [
+          Expanded(
+            child: Center(
+              child: Image.asset('assets/images/bg.png', fit: BoxFit.cover),
+            ),
+          ),
+          Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton.icon(
+                    onPressed: () {
+                      GoRouter.of(context).go('/play');
+                    },
+                    icon: const Icon(Icons.play_arrow),
+                    label: const Text('Play'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton.icon(
+                    onPressed: () {
+                      GoRouter.of(context).go('/tutorial');
+                    },
+                    icon: const Icon(Icons.help),
+                    label: const Text('Instructions'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton.icon(
+                    onPressed: () {
+                      GoRouter.of(context).go('/settings');
+                    },
+                    icon: const Icon(Icons.settings),
+                    label: const Text('Settings'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton.icon(
+                    onPressed: () {
+                      // Exit app
+                      SystemNavigator.pop();
+                    },
+                    icon: const Icon(Icons.exit_to_app),
+                    label: const Text('Exit'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Align(
+            alignment: Alignment.bottomRight,
+            child: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: PayPalButton(
+                paypalButtonId: "VYTU88VHK2QD4",
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton.icon(
-                onPressed: () {
-                  GoRouter.of(context).go('/tutorial');
-                },
-                icon: const Icon(Icons.help),
-                label: const Text('Instructions'),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton.icon(
-                onPressed: () {
-                  GoRouter.of(context).go('/settings');
-                },
-                icon: const Icon(Icons.settings),
-                label: const Text('Settings'),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton.icon(
-                onPressed: () {
-                  // Exit app
-                  SystemNavigator.pop();
-                },
-                icon: const Icon(Icons.exit_to_app),
-                label: const Text('Exit'),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
